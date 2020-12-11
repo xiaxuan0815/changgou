bc5a1c724a6b590eb5715bf1e6e1ca63063c269d
3158438026@qq.com <3158438026@qq.com>
2020-12-11 21:37:30 +0800

    提交12.11

---
+++ changgou-service/changgou-service-goods/src/main/java/com/changgou/goods/dao/ActivityMapper.java
@@ -0,0 +1,12 @@
+package com.changgou.goods.dao;
+
+import com.changgou.goods.pojo.Activity;
+import tk.mybatis.mapper.common.Mapper;
+
+/****
+ * @Author:shenkunlin
+ * @Description:Activity的Dao
+ * @Date 2019/6/14 0:12
+ *****/
+public interface ActivityMapper extends Mapper<Activity> {
+}
---
+++ changgou-service/changgou-service-goods/src/main/java/com/changgou/goods/dao/OrderMapper.java
@@ -0,0 +1,12 @@
+package com.changgou.goods.dao;
+
+import com.changgou.goods.pojo.Order;
+import tk.mybatis.mapper.common.Mapper;
+
+/****
+ * @Author:shenkunlin
+ * @Description:Order的Dao
+ * @Date 2019/6/14 0:12
+ *****/
+public interface OrderMapper extends Mapper<Order> {
+}
